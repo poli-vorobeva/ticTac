d1ec195e6bc4f93eae74f96c40adfbbf66b87b30
poli8512 <poli8512@gmail.com>
2023-02-18 15:50:48 +0300

    feat: add styles

--- src/tabs/Task2.ts
+++ src/tabs/Task2.ts
@@ -1,8 +1,31 @@
 import Control from "../controll";
+import styles from './task2.module.css'
 
 export default class Task2 extends Control {
 	constructor(parent: HTMLElement) {
-		super(parent);
-		new Control(this.node, 'h3', '', 'task2')
+		super(parent,'div',styles.task2Wrapper);
+		const title=`Сравните две статьи (одна поновее, вторая — прошлогодняя) и расскажите, что изменилось в лучшую или худшую сторону, поделитесь своими общими впечатлениями, скажите, что вы оставили бы, а что стоит изменить (и почему)`
+		const data=[
+			{link: 'https://korabli.su/ru/news/game-updates/update-0910-american-battleships/',
+				description:`На мой взгляд слишком большая вступительная часть. Я бы не стала скроллить до новости,хотелось бы, чтобы хоть
+	мини-кусочек ее было видно сразу. Один из двух баннеров
+	я бы убрала, или если они важны, то можно было бы сделать их бесконечным слайером со сменой кадров по таймеру * 
+	Для меня- новость слишком длинная- я бы разбила ее на подразделы, что-то вроде раскрывающихся подразделов, так как 
+	очень много текста и картинок, там более, что явно можно выделить логические блоки`},
+			{link:'https://korabli.su/ru/news/game-updates/update-01111-new-year/',
+				description: `Мне не нравится меню между баннерами, оно теряется между ними. непонятно, что оно относится к новости.
+		Я бы по прежнему- убрала один баннер. и меню перенесла к новости, тогда бы понятнее * 
+		Я думаю, что кнопки слайдера, не должны наезжать на футер`}
+		]
+		new Control(this.node, 'h3', '', title)
+		data.forEach(d=>{
+			const wr= new Control(this.node,'div',styles.itemWrapper)
+			const link = new Control(wr.node,'p','',d.link)
+			const desc= new Control(wr.node,'div')
+			d.description.split('*').forEach(s=>{
+				new Control(desc.node,'p','',s)
+			})
+
+		})
 	}
 }
--- src/tabs/Task3.ts
+++ src/tabs/Task3.ts
@@ -1,8 +1,42 @@
 import Control from "../controll";
+import styles from './task3.module.css'
 
 export default class Task3 extends Control {
 	constructor(parent: HTMLElement) {
-		super(parent);
-		new Control(this.node, 'h3', '', 'task3')
+		super(parent, 'div', styles.task3);
+		const dags = [
+			{
+				text: 'раздел-единый аккаунт (на 420px ) - cдвигаются картинки',
+				photo: 'account'
+			},
+			{
+				text: 'пропадает кнопка прокрутки вверх ( меньше 800px)',
+				photo: 'scroll'
+			},
+			{
+				text: 'в разделе галлерея обрезается вторая картинка, можно сделать слайдер( меньше 800px)',
+				photo: 'gallery'
+			},
+			{
+				text: 'баннеры в самом верху- не работает кнопка play- вводит в заблуждение. По клику просто открывается картинка( меньше 500px)',
+				photo: 'banner'
+			}]
+		const inHtml = `Теги скрипт находятся внутри блоков див *
+	 В некоторых местах див обернут в див, без функциональности*
+	Теги скрипт лежит в дивах по документу`
+		new Control(this.node, 'h3', '', ` Рассмотрите и сделайте анализ страницы «Об игре», перечислите ошибки в дизайне, вёрстке, как это можно улучшить, почему и какой эффект от улучшения можно получить.`)
+		const bagsWrapper = new Control(this.node, 'div', styles.bgsWrpr)
+		dags.forEach(b => {
+			const bagWrapper = new Control(bagsWrapper.node, 'figure', styles.figure)
+			const img: Control<HTMLImageElement> = new Control(bagWrapper.node, 'img')
+			img.node.src = `./public/assets/${b.photo}.png`
+			img.node.width = 200
+			const figcaption = new Control(bagWrapper.node, 'figcaption', '', b.text)
+		})
+		const htmlWr= new Control(this.node,'div',styles.inHt)
+		new Control(htmlWr.node,'h4','','В Верстке: ')
+		inHtml.split('*').forEach(el=>{
+			new Control(htmlWr.node,'p','',el)
+		})
 	}
 }
